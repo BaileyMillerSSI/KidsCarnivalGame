af1ca3522a5973c8ed4241cbcf879d1aba089d5c
Bailey Miller <bjm19@pct.edu>
2018-12-27 15:09:39 -0500

    Created a simple title screen with exit and start buttons and scene transition to playing

---
+++ Unity Game/Pizza Baker/Assets/Scripts/TitleScreenEventManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenEventManager: MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void StartNewGame(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+}
